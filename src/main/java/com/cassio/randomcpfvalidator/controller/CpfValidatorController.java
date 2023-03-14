179cb922f91f4344791a7de9b780c9e573c93bc9
Cássio Ribeiro Pereira <cassio@gomining.com.br>
2023-03-14 11:17:30 -0300

    add dockerfile

--- src/main/java/com/cassio/randomcpfvalidator/controller/CpfValidatorController.java
+++ src/main/java/com/cassio/randomcpfvalidator/controller/CpfValidatorController.java
@@ -1,10 +1,11 @@
 package com.cassio.randomcpfvalidator.controller;
 
 import br.com.caelum.stella.validation.CPFValidator;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Random;
 
